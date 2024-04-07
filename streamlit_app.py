7aac0203d1264951696cb485c5e607e907a42387
Mahantesh-SnowAdmin <166269025+Mahantesh-SnowAdmin@users.noreply.github.com>
2024-04-07 12:41:46 +0530

    Update streamlit_app.py
    final touch

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,8 +15,14 @@ from snowflake.snowpark.functions import col
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME').col('search_on'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop()
+
+#Convert the snowpark datafrane to a pandas dataframe so we can use LOC function
+pd_df = my_dataframe.to_pandas()
+st.dataframe = (pd_df)
+st.stop()
 
 ingredients_list=st.multiselect(
     'Choose upto five ingredients',
@@ -29,6 +35,9 @@ if ingredients_list:
     
     for friut_choosen in ingredients_list:
         ingredients_string += friut_choosen + ' '
+        
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
 
     st.write(ingredients_string)
 
