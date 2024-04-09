ad399d17cf8fbbd0d52e1059bc65e1f07d75d1d3
Mahantesh-SnowAdmin <166269025+Mahantesh-SnowAdmin@users.noreply.github.com>
2024-04-09 13:09:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,3 +55,25 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success("✅ Your Smoothie is ordered, " + name_on_order + "!")
+        
+session = get_active_session()
+my_dataframe = session.table("smoothies.public.ORDERS").filter(col('ORDER_FILLED')==0).collect()
+
+if my_dataframe:
+    editable_df = st.data_editor(my_dataframe)
+    submitted = st.button('Submit')
+    if submitted:
+        og_dataset = session.table("smoothies.public.orders")
+        edited_dataset = session.create_dataframe(editable_df)
+        st.success('Someone Clicked the Button',icon = "👍")
+        try:
+            og_dataset.merge(edited_dataset
+                     , (og_dataset['order_uid'] == edited_dataset['order_uid'])
+                     , [when_matched().update({'ORDER_FILLED': edited_dataset['ORDER_FILLED']})]
+                    )
+            st.success('Order(s) Updataed',icon = "👍")
+        except:
+            st.write('Someone went Wrong.')
+    
+else:
+    st.success('There is no pending Orders right Now',icon = "👍")
