e994bf781b96367dfe4c0378ad07d3be2c517b05
Mahantesh-SnowAdmin <166269025+Mahantesh-SnowAdmin@users.noreply.github.com>
2024-04-14 15:25:45 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,8 +22,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 
 #Convert the Snowpark dataframe to a Pandas Dataframe so we can use the Loc function
 pd_df = my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+#st.dataframe(pd_df)
+#st.stop()
 
 ingredients_list=st.multiselect(
     'Choose upto 5 ingredients',
@@ -36,12 +36,16 @@ if ingredients_list:
     
     for friut_choosen in ingredients_list:
         ingredients_string += friut_choosen + ' '
-
         
+
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        
+        st.subheader(friut_choosen + 'Nutrition Information')
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + friut_choosen)
+        fd_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
-    st.write(ingredients_string)
+st.write(ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string + """','""" +name_on_order + """')"""
@@ -54,13 +58,3 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success("✅"'Your Smoothie is ordered,'+ name_on_order +'!')
 
-
-import requests
-
-if ingredients_list:
-    ingredients_string=''
-    for friut_choosen in ingredients_list:
-        ingredients_string += friut_choosen + ' '
-        st.subheader(friut_choosen + 'Nutrition Information')
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + friut_choosen)
-        fd_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
