8a51b5712bc9d501b7cfaad44c709bac0a00371e
Mahantesh-SnowAdmin <166269025+Mahantesh-SnowAdmin@users.noreply.github.com>
2024-04-14 15:28:10 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,6 @@ if ingredients_list:
     for friut_choosen in ingredients_list:
         ingredients_string += friut_choosen + ' '
         
-
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         
